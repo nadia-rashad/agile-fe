11bafe972c55bdd0f570a9a371e04203c8b7df60
Nadia Rashad <r027334k@student.staffs.ac.uk>
2022-08-01 13:18:20 +0100

    upgraded to react-router-dom

--- skills_auditor_fe/src/App.js
+++ skills_auditor_fe/src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
 import * as api from './api';
 import React from 'react';
-import { Router } from '@gatsbyjs/reach-router';
 import Home from './components/Home';
 import StaffAddSkill from './components/staff/StaffAddSkill';
 import StaffViewEditSkill from './components/staff/StaffViewEditSkill';
 import StaffDetails from './components/staff/StaffDetails';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
         <Router>
-         <Home path="/" />
-         <StaffAddSkill path='/staff/add_skill'/>
-         <StaffViewEditSkill path='/staff/view_edit_skill'/>
-         <StaffDetails path='/staff/my_details'/>
+        <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path='/staff/view_edit_skill' element={<StaffViewEditSkill/>} />
+        <Route exact path='/staff/add_skill' element={<StaffAddSkill/>} />
+        <Route exact path='/staff/my_details' element={<StaffDetails/>} />
+        </Routes>
         </Router>
     </div>
   );
--- skills_auditor_fe/src/components/Home.jsx
+++ skills_auditor_fe/src/components/Home.jsx
@@ -1,9 +1,12 @@
-
+import React from 'react';
 
 function Home() {
 
     return(
+        <div>
         <h1>Homepage</h1>
+
+        </div>
     )
 
 }
