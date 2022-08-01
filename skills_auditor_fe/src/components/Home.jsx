f8e078d907b93839a1ffd5a0915f477313b0c5f7
Nadia Rashad <r027334k@student.staffs.ac.uk>
2022-08-01 13:20:24 +0100

    added quicklink to staff page

--- skills_auditor_fe/src/components/Home.jsx
+++ skills_auditor_fe/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
 
     return(
         <div>
         <h1>Homepage</h1>
+        <ul>
+        <li>
+          <Link to="/staff/my_details">Staff my details page</Link>
+        </li>
+        </ul>
 
         </div>
     )
