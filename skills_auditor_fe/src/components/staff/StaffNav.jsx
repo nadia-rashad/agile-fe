46bdfd096289a5884bac8ec760c8fd16b28952dc
Nadia Rashad <r027334k@student.staffs.ac.uk>
2022-08-01 12:44:12 +0100

    added log out link

--- skills_auditor_fe/src/components/staff/StaffNav.jsx
+++ skills_auditor_fe/src/components/staff/StaffNav.jsx
@@ -6,10 +6,14 @@ function StaffNav() {
     return (
         <div >
         <Nav defaultActiveKey="/" className="Container">
-          <Nav.Link href="/my_details">My Information</Nav.Link>
+          <Nav.Link href="/my_details"  className="NavLinks">My Information</Nav.Link>
           <h3 className="Header">Skills</h3>
           <Nav.Link href="/view_edit_skill">View/ Edit Skills</Nav.Link>
-          <Nav.Link href="/add_skill">Add Skill</Nav.Link>
+          <Nav.Link href="/add_skill" >Add Skill</Nav.Link>
+          <br></br>
+         
+        
+          <Nav.Link href="/home" className='LogOut'>Log out</Nav.Link>
         </Nav>
         </div>
       );
