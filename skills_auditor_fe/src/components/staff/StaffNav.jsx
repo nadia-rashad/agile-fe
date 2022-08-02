9dbca2f39ac345688446d00b9de012113796bb00
Nadia Rashad <r027334k@student.staffs.ac.uk>
2022-08-02 08:12:56 +0100

    added accessibility tags

--- skills_auditor_fe/src/components/staff/StaffNav.jsx
+++ skills_auditor_fe/src/components/staff/StaffNav.jsx
@@ -5,11 +5,11 @@ function StaffNav() {
     
     return (
         <div >
-        <Nav defaultActiveKey="/" className="Container">
-          <Nav.Link href="/staff/my_details"  className="NavLinks">My Information</Nav.Link>
+        <Nav defaultActiveKey="/" className="Container" role="navigation" aria-label="Staff Navigation" >
+          <Nav.Link href="/staff/my_details"  className="NavLinks" role="link">My Information</Nav.Link>
           <h3 className="Header">Skills</h3>
-          <Nav.Link href="/staff/view_edit_skill">View/ Edit Skills</Nav.Link>
-          <Nav.Link href="/staff/add_skill" >Add Skill</Nav.Link>
+          <Nav.Link href="/staff/view_edit_skill" role="link">View/ Edit Skills</Nav.Link>
+          <Nav.Link href="/staff/add_skill" role="link" >Add Skill</Nav.Link>
           <br></br>
          
         
