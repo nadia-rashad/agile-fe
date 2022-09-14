b89b987ef2d721b734708a9036eafe978db35a5b
TRT-ArchieSD <archie.sd@raytheon.co.uk>
2022-09-14 15:47:47 +0100

    Added testing for manager staffskills page

--- skills_auditor_fe/src/components/testing/test_data.js
+++ skills_auditor_fe/src/components/testing/test_data.js
@@ -10,6 +10,18 @@ export const userDetails = { details :  {
     title: "Mr",
 }};
 
+export const managerUserDetails = { details :  {
+    id: 3,
+    firstName: "Nadia",
+    surname: "Rashad",
+    title: "Mrs",
+    email: "nadia@test.com",
+    job_role_id: 3,
+    password: "abc",
+    system_role_id: 1,
+    manager_id: 5
+}};
+
 export const skillResp = {
     data: [
       { category_id: 2, description: "Javascript", id: 4 },
@@ -21,36 +33,37 @@ export const skillResp = {
 export const assignedSkillsResp = {
     data: [
         { 
-        expiry_date: "2022-08-26",
-        id: 33,
-        skill_id: 4,
-        staff_id: 2,
-        strength: "Expert" 
+            expiry_date: "2022-08-26",
+            id: 33,
+            skill_id: 4,
+            staff_id: 2,
+            strength: "Expert" 
         },
-    {
-        expiry_date: "2022-08-26",
-        id: 34,
-        skill_id: 6,
-        staff_id: 2,
-        strength: "Intermediate"
-    }
+        {
+            expiry_date: "2022-08-26",
+            id: 34,
+            skill_id: 6,
+            staff_id: 2,
+            strength: "Intermediate"
+        }
     ]
 }
 
 export const skillTableData = {
     data : [
-    {
-        expiryData: "2022-08-26",
-        skillCategory: "Testing Tools",
-        skillId: 35,
-        skillName: "Excel",
-        skillStrength: "Basic"},
+        {
+            expiryData: "2022-08-26",
+            skillCategory: "Testing Tools",
+            skillId: 35,
+            skillName: "Excel",
+            skillStrength: "Basic"},
         {
             expiryData: "2023-01-25",
             skillCategory: "Testing Tools",
             skillId: 37,
             skillName: "Terraform",
-            skillStrength: "Intermediate"}
+            skillStrength: "Intermediate"
+        }
     ]
 }
 
@@ -73,4 +86,31 @@ export const newSkill = {
         skillName: "Word",
         skillStrength: "Basic"
     }
+}
+
+export const assignedStaff = {
+    data : [
+        {
+            "id": 1,
+            "firstName": "Harry",
+            "surname": "Styles",
+            "title": "Mr",
+            "email": "harry@email.com",
+            "job_role_id": 2,
+            "password": "abc",
+            "system_role_id": 2,
+            "manager_id": 3
+        },
+        {
+            "id": 5,
+            "firstName": "Archie",
+            "surname": "Sanger-Davies",
+            "title": "Mr",
+            "email": "archie@test.coooom",
+            "job_role_id": 3,
+            "password": "abc",
+            "system_role_id": 1,
+            "manager_id": 3
+        }
+    ]
 }
